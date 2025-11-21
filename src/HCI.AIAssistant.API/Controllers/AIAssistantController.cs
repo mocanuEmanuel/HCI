eb5d0da4c85d06d40b2964990ee9e4a607219e17
mocanuEmanuel <mocanuemanuel19@proton.me>
2025-11-21 13:47:47 +0200

    lab2

--- src/HCI.AIAssistant.API/Controllers/AIAssistantController.cs
+++ src/HCI.AIAssistant.API/Controllers/AIAssistantController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 
 using HCI.AIAssistant.API.Models.DTOs.AIAssistantController;
+using HCI.AIAssistant.API.Services;
+using HCI.AIAssistant.API.Models.DTOs;
 
 namespace HCI.AIAssistant.API.Controllers;
 
@@ -8,12 +10,42 @@ namespace HCI.AIAssistant.API.Controllers;
 [Route("api/[controller]")]
 public class AIAssistantController : ControllerBase
 {
-    [HttpPost("/message")]
-    public async Task<ActionResult<AIAssistantControllerPostMessageResponseDTO>> PostMessage([FromBody] AIAssistantControllerPostMessageRequestDTO request)
+    private readonly IAIAssistantService _aIAssistantService;
+    private readonly IParametricFunctions _parametricFunctions;
+
+    public AIAssistantController(
+        IAIAssistantService aIAssistantService,
+        IParametricFunctions parametricFunctions
+    )
     {
+        _aIAssistantService = aIAssistantService;
+        _parametricFunctions = parametricFunctions;
+    }
+
+    [HttpPost("/message")]
+    [ProducesResponseType(typeof(AIAssistantControllerPostMessageResponseDTO), 200)]
+    [ProducesResponseType(typeof(ErrorResponseDTO), 400)]
+    public async Task<ActionResult> PostMessage([FromBody] AIAssistantControllerPostMessageRequestDTO request)
+    {
+        if (!_parametricFunctions.ObjectExistsAndHasNoNullPublicProperties(request))
+        {
+            return BadRequest(
+                new ErrorResponseDTO()
+                {
+                    TextErrorTitle = "AtLeastOneNullParameter",
+                    TextErrorMessage = "Some parameters are null/missing.",
+                    TextErrorTrace = _parametricFunctions.GetCallerTrace()
+                }
+            );
+        }
+
+#pragma warning disable CS8604
+        string textMessageResponse = await _aIAssistantService.SendMessageAndGetResponseAsync(request.TextMessage);
+#pragma warning restore CS8604
+
         AIAssistantControllerPostMessageResponseDTO response = new()
         {
-            TextMessage = "HI!" + request.TextMessage
+            TextMessage = textMessageResponse
         };
 
         return Ok(response);
---
+++ src/HCI.AIAssistant.API/Services/IParametricFunctions.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HCI.AIAssistant.API.Services
+{
+    public interface IParametricFunctions
+    {
+        bool ObjectExistsAndHasNoNullPublicProperties(object? obj);
+
+        string GetCallerTrace(
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerFilePath] string filePath = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int lineNumber = 0);
+    }
+}
